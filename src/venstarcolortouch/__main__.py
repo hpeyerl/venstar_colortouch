3554d4b30a672cb016ee91448af577c90ff9b8a6
Dan Cinnamon <dcinnamon@gmail.com>
2018-01-10 20:13:50 -0600

    Added the following: 1) SSL + authentication support 2) Helper methods for HASS integration 3) Extra error logging.

--- src/venstarcolortouch/__main__.py
+++ src/venstarcolortouch/__main__.py
@@ -12,10 +12,23 @@ def test():
         return False
 
     a = sys.argv[1]
+    
+    if len(sys.argv) >= 3: 
+        proto = sys.argv[2]
+    else:
+        proto = 'http'
+
+    if len(sys.argv) == 5:
+        user = sys.argv[3]
+        pwd = sys.argv[4]
+    else:
+        user = None
+        pwd = None
+    
     print ("Testing with IP: {0}".format(a))
 
-    ct = venstarcolortouch.VenstarColorTouch(a, timeout=5)
-
+    ct = venstarcolortouch.VenstarColorTouch(a, timeout=5, proto=proto, user=user, password=pwd)
+    print("logging in...")
     if ct.login() is True:
         print("Login successful. API: {0} Type: {1}".format(ct._api_ver,ct._type))
 
@@ -31,18 +44,18 @@ def test():
     else:
         print("Was not able to get sensors")
 
-    print("Indoor temp is {t} and humidity is {h}".format(t=ct.get_thermostat_sensor("temp"),h=ct.get_thermostat_sensor("hum")))
+    print("Indoor temp is {t} and humidity is {h}".format(t=ct.get_indoor_temp(),h=ct.get_indoor_humidity()))
 
     print("Runtimes: {0}".format(ct.get_runtimes()))
 
     curh=ct.get_info("heattemp")
     curc=ct.get_info("cooltemp")
-    ct.set_setpoints(60,90)
-    ct.update_info()
-    print("Heat setpoint is {h}\nCool setpoint is {c}\n".format(h=ct.get_info("heattemp"),c=ct.get_info("cooltemp")))
-    ct.set_setpoints(curh,curc)
-    ct.update_info()
-    print("Heat setpoint is {h}\nCool setpoint is {c}\n".format(h=ct.get_info("heattemp"),c=ct.get_info("cooltemp")))
+    #ct.set_setpoints(60,90)
+    #ct.update_info()
+    #print("Heat setpoint is {h}\nCool setpoint is {c}\n".format(h=ct.get_info("heattemp"),c=ct.get_info("cooltemp")))
+    #ct.set_setpoints(curh,curc)
+    #ct.update_info()
+    #print("Heat setpoint is {h}\nCool setpoint is {c}\n".format(h=ct.get_info("heattemp"),c=ct.get_info("cooltemp")))
 
     return True
 
